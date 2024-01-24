730b85eff3cc44bc2d1d53b05f68a61913c8e877
hereforhomework <anhduy2711415@gmail.com>
2024-01-24 08:32:12 +0700

    V1.0: Adding product page

--- dexStarter/dex/src/components/Header.js
+++ dexStarter/dex/src/components/Header.js
@@ -1,33 +1,42 @@
-import React from 'react'
-import Eth from '../eth.svg'
-import Logo from '../Logo.png'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import Eth from '../eth.svg';
+import Logo from '../Logo.png';
+import { Link, useLocation } from 'react-router-dom';
+
 function Header() {
+  const location = useLocation();
+  const isStorePage = location.pathname === '/store';
+
   return (
     <header>
-      <div class="leftH">
+      <div className="leftH">
         <img src={Logo} className="logo" alt="logo" />
 
-        {/* link to the item */}
         <Link to="/" className="link">
-          <div className='headerItem'>Swap</div>
+          <div className="headerItem">Swap</div>
         </Link>
         <Link to="/tokens" className="link">
-          <div className='headerItem'>Token </div>
+          <div className="headerItem">Token </div>
+        </Link>
+        <Link to="/store" className="link">
+          <div className="headerItem">Store </div>
         </Link>
       </div>
-
-      <div class="rightH">
+      
+      <div className="rightH">
+        {isStorePage && (
+            <div className="search-bar">
+              <input type="text" id="search-bar-input" placeholder="Search..." />
+            </div>
+          )}
         <div className="headerItem">
           <img src={Eth} alt="eth" className="eth" />
           Etherium
         </div>
-        <div className="connectButton" >
-          Connect
-        </div>
+        <div className="connectButton">Connect</div>
       </div>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
