aa0063573ff61b57b9f9af81d05ca280995a0ee8
hereforhomework <118524035+hereforhomework@users.noreply.github.com>
2024-03-08 14:27:06 +0700

    Create Database.sql

---
+++ Database/Database.sql
@@ -0,0 +1,37 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+CREATE TABLE `assets` (
+  `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+  `name` varchar(255) NOT NULL,
+  `description` text DEFAULT NULL,
+  `current_price` decimal(15,2) NOT NULL,
+  `image_url` varchar(1024) DEFAULT NULL,
+  `category` varchar(255) NOT NULL DEFAULT 'NFT',
+  `availability` boolean DEFAULT true
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `email` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `balance` int(11) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+
+CREATE TABLE `transactions` (
+  `transaction_id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `user_id` int(11) NOT NULL DEFAULT 1,
+  `asset_id` int(11) NOT NULL, 
+  `hash` char(64) NOT NULL,
+  `received` timestamp NOT NULL DEFAULT current_timestamp(),
+  `status` enum('Pending','Confirmed','Denied') DEFAULT 'Pending',
+  CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_asset_id` FOREIGN KEY (`asset_id`) REFERENCES `assets` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
