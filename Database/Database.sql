cca1364efe1fcd1dcc5e2d837581dc8a077236bb
hereforhomework <118524035+hereforhomework@users.noreply.github.com>
2024-03-08 21:11:39 +0700

    Update Database.sql

--- Database/Database.sql
+++ Database/Database.sql
@@ -1,36 +1,29 @@
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
 CREATE TABLE `assets` (
-  `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
-  `name` varchar(255) NOT NULL,
-  `description` text DEFAULT NULL,
-  `current_price` decimal(15,2) NOT NULL,
-  `image_url` varchar(1024) DEFAULT NULL,
-  `category` varchar(255) NOT NULL DEFAULT 'NFT',
-  `availability` boolean DEFAULT true
+  `id` INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `name` VARCHAR(255) NOT NULL,
+  `description` TEXT DEFAULT NULL,
+  `current_price` DECIMAL(15,2) NOT NULL,
+  `image_url` VARCHAR(1024) DEFAULT NULL,
+  `category` VARCHAR(255) NOT NULL DEFAULT 'NFT',
+  `availability` BOOLEAN DEFAULT TRUE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-
 
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `email` varchar(255) NOT NULL,
-  `username` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `balance` int(11) DEFAULT NULL,
+  `id` INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `email` VARCHAR(255) NOT NULL UNIQUE,
+  `username` VARCHAR(255) NOT NULL UNIQUE,
+  `password` VARCHAR(255) NOT NULL,
+  `balance` DECIMAL(15,2) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-
-
 CREATE TABLE `transactions` (
-  `transaction_id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `user_id` int(11) NOT NULL DEFAULT 1,
-  `asset_id` int(11) NOT NULL, 
-  `hash` char(64) NOT NULL,
-  `received` timestamp NOT NULL DEFAULT current_timestamp(),
-  `status` enum('Pending','Confirmed','Denied') DEFAULT 'Pending',
+  `transaction_id` INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `user_id` INT(11) NOT NULL,
+  `asset_id` INT(11) NOT NULL, 
+  `hash` CHAR(64) NOT NULL,
+  `received` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+  `status` ENUM('Pending','Confirmed','Denied') DEFAULT 'Pending',
   CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `fk_asset_id` FOREIGN KEY (`asset_id`) REFERENCES `assets` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `fk_asset_id` FOREIGN KEY (`asset_id`) REFERENCES `assets` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  INDEX (`user_id`, `asset_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
